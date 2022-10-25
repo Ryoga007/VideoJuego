7520d5e17b100d5fd0fecc02a235c726ed7aef9e
pepe <Ryogahp@hotmail.com>
2022-10-25 11:21:40 -0400

    Agregando lo que fataba del video juego

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ kaboom({
 })
 // *Cargando imagenes estaticas
 loadSprite('imgFondoEscena0', 'assets/sprites/wallpaperbetter.jpg')
+loadSprite('imgFondoEscena1', 'assets/sprites/wallpaperbetter1.jpg')
+loadSprite('imgFondoEscena2', 'assets/sprites/wallpaperbetter1.jpg')
+loadSprite('imgIntroLinia', 'assets/sprites/Linia.png')
+loadSprite('imgDocente', 'assets/sprites/docente.png')
+loadSprite('mapa','assets/sprites/mapaAset01.png')
+loadSprite('block', 'assets/sprites/block1.png' )
+loadSprite('cebra', 'assets/sprites/cono.png')
+loadSprite('logoUsfa', 'assets/sprites/LogoUsfa.png')
 
-// *caragando imagenes con movimiento
+// *Cargamos sonido
+
+// *Caragando imagenes con movimiento
 loadSpriteAtlas('assets/sprites/AutoSet01t.png', {
     hero: {
       x: 1,
@@ -20,23 +30,15 @@ loadSpriteAtlas('assets/sprites/AutoSet01t.png', {
         run: { from: 0, to: 1, loop: true, speed: 5 }
       }
     },
-  //   block: {
-  //     x: 1,
-  //     y: 1,
-  //     width: 30,
-  //     height: 30,
-  //     sliceX: 1
-  //   },
-  //   cebra: {
-  //     x: 1,
-  //     y: 1,
-  //     width: 30,
-  //     height: 30,
-  //     sliceX: 1
-  //   }
   })
+
+// *Declarando las Variables
+// const music = play('musicaFondo', {loop: true, volume: 1})
+var linea;
+var opcionMenu = 1;
   
 
+// * Iniciando las escenas de incio
 
 scene('credits-0', () => {
     wait(1, () => {
@@ -61,18 +63,28 @@ scene('credits-1', () => {
 		sprite('imgFondoEscena0')
 	])
 
-	wait(4, () => {
+  wait(1, () => {
+    add([
+      sprite('logoUsfa'),
+      pos(width()/2, height()/4),
+      scale(0.5),
+      origin('center')
+    ])
+  })
+
+	wait(3, () => {
 	  add([  
-		text('Producido por su servidor', {
-		  size: 28,
+		text('Proyecto de grado', {
+		  size: 50,
 		  font: 'apl386'
 		}),
 		pos(width()/2, height()/2),
+    color(27, 38, 49),
 		origin('center')
 	  ]);
 	})
   
-	wait(7, () => {
+	wait(5, () => {
 	  add([
 		pos(width()/2, (height() - (height()*0.4))),
 		sprite("hero", {anim: 'run'}),
@@ -84,10 +96,455 @@ scene('credits-1', () => {
 	  ])
 	})
   
-	wait(13, () => {
-	  go('intro-1')
+	wait(8, () => {
+	  go('menu')
 	})
 })
 
+// * Esla ecena del selecion de menu
+scene('menu', () => {
+	add([
+		sprite('imgFondoEscena1')
+	]),
+	wait(1, () => {
+		add([
+			text('Iniciar Juego', {
+			  size: 45,
+			  font: 'apl386'
+			}),
+			pos(width()/2, 250),
+			origin('center')
+		]);
+	}),
+	wait(2, () => {
+		add([
+			text('Instrucciones', {
+			  size: 45,
+			  font: 'apl386'
+			}),
+			pos(width()/2, 400),
+			origin('center')
+		]);
+	}),
+	wait(3, () => {
+		add([
+			text('Salir de juego', {
+			  size: 45,
+			  font: 'apl386'
+			}),
+			pos(width()/2, 550),
+			origin('center')
+		]);
+	})
+
+	wait(4, () => {
+		linea = add([
+			sprite('imgIntroLinia'),
+			pos(width()/2, 280),
+			origin('center')
+		]);
+
+		onKeyPress("enter", () => {
+			switch (opcionMenu) {
+				case 1:
+					// music.stop(),
+					go("main",0)
+					break;
+				case 2:
+					go("informacion")
+					break;
+				default:
+					go('credits-0')
+					break;
+			}
+		})
+	})
+
+	onKeyPress("down", () => {
+
+		switch (opcionMenu) {
+			case 1:
+				linea.destroy();
+				linea = add([
+					sprite('imgIntroLinia'),
+					pos(width()/2, 430),
+					origin('center')
+				]);
+				opcionMenu = 2;
+				break;
+			case 2:
+				linea.destroy();
+				linea = add([
+					sprite('imgIntroLinia'),
+					pos(width()/2, 580),
+					origin('center')
+				]);
+				opcionMenu = 3;
+				break;
+			default:
+				linea.destroy();
+				linea = add([
+					sprite('imgIntroLinia'),
+					pos(width()/2, 280),
+					origin('center')
+				]);
+				opcionMenu = 1;
+				break;
+		}
+	})
+
+	onKeyPress("up", () => {
+
+		switch (opcionMenu) {
+			case 1:
+				linea.destroy();
+				linea = add([
+					sprite('imgIntroLinia'),
+					pos(width()/2, 580),
+					origin('center')
+				]);
+				opcionMenu = 3;
+				break;
+			case 2:
+				linea.destroy();
+				linea = add([
+					sprite('imgIntroLinia'),
+					pos(width()/2, 280),
+					origin('center')
+				]);
+				opcionMenu = 1;
+				break;
+			default:
+				linea.destroy();
+				linea = add([
+					sprite('imgIntroLinia'),
+					pos(width()/2, 430),
+					origin('center')
+				]);
+				opcionMenu = 2;
+				break;
+		}
+	})
+
+	
+}),
+
+scene("informacion", () => {
+	add([
+		sprite('imgFondoEscena2')
+	]),
+    
+    wait(1, () => {
+        add([
+			pos(width()/1.7, height()/2),
+			rect(1140, 690),
+			color(83, 39, 56),
+            origin('center')
+			// z(100),
+		])
+		add([
+			pos(width()/1.7, height()/2),
+			rect(1100, 650),
+			color(244, 246, 247),
+            origin('center')
+			// z(100),
+		])
+	})
+
+    wait(2, () => {
+		add([
+			text('Instrucciones\ndel Juego ', {
+			  size: 60,
+			  font: 'apl386',
+			}),
+			color(23, 32, 42),
+			pos(width()/1.7, height()/6),
+			origin('center')
+		])
+	})
+
+    wait(3, () => {
+		add([
+			sprite('imgDocente'),
+			pos(width()/4, height()/2),
+            scale(0.8),
+			origin('center')
+		])
+	})
+
+	wait(9, () => {
+		add([
+		  text('precione ESPACIADOR para continuar', {
+			size: 22,
+			font: 'apl386'
+		  }),
+		  pos(width()/2, height() - (height()*0.1)),
+		  origin('center')
+		]);
+	})
+	  
+	onKeyPress("space", () => {
+		console.log(onKeyPress);
+		go("menu")
+	});
+})
+
+// *Empieza el Juego
+
+scene("main", (levelIdx) => {
+
+	// * Empesacos a CARGAR la pagina con el 'mapa' y la musica de fondo
+	add([
+		sprite('mapa'),
+		scale(4,4)
+	])
+
+	// const music = play("musicaFondo", {
+	// 	volume: 0.5,
+	// 	loop: true
+	// })
+
+	
+	const SPEED = 320
+
+	const characters = {
+		"a": {
+			sprite: "cebra",  //!Cambio "bag" por "cebra"
+			msg: "Es un paso de Cebra, recuerda mirar a ambos lados",
+		},
+		// "b": {
+		// 	sprite: "ghosty",
+		// 	msg: "get out!",
+		// },
+	}
+
+
+	const levels = [
+		[
+			" ",    
+			" ",
+			" =======================================================================================",
+			" =  @        a                                                                         =",
+			" =                                                                                     =",
+			" =                                                                                     =",
+			" =                                                                                     =",
+			" =                                                                                     =",
+			" =           a                                                                         =",
+			" ====       =============      =============      ==============      ==============   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			" ====       =============      =============      ==============      ==============   =",
+			" =                                                                                     =",
+			" =                                                                                     =",
+			" =                                                                                     =",
+			" =                                                                                     =",
+			" =                                                                                     =",
+			" =                                                                                     =",
+			" ====       =============      =============      ==============      ==============   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =       =           =      =           =      =            =      =            =   =",
+			"    =========           ========           ========            ========            =====",
+		],
+		[
+			"---------",
+			"-       -",
+			"-       -",
+			"-  $    -",
+			"|       -",
+			"-       -",
+			"-     a -",  //!cambiando al bicho b por a
+			"-   @   -",
+			"---------",
+		],
+	]
+
+	addLevel(levels[levelIdx], {
+		width: 36,
+		height: 36,
+		// pos: vec2(64, 64),
+		"=": () => [
+			sprite("block"), //!cambiando "grass" por "block"
+			area(),
+			solid(),
+		],
+		// "-": () => [
+		// 	sprite("steel"),
+		// 	area(),
+		// 	solid(),
+		// ],
+		// "$": () => [
+		// 	sprite("key"),
+		// 	area(),
+		// 	"key",
+		// ],
+		"@": () => [
+			sprite("hero"), //!cambiando "bean" por "hero"
+			area(),
+			solid(),
+			scale(3),
+			"player",
+		],
+		// "|": () => [
+		// 	sprite("door"),
+		// 	area(),
+		// 	solid(),
+		// 	"door",
+		// ],
+
+		any(ch) {
+			const char = characters[ch]
+			if (char) {
+				return [
+					sprite(char.sprite),
+					area(),
+					solid(),
+					"character",
+					{ msg: char.msg, },
+				]
+			}
+		},
+	})
+
+	
+	const player = get("player")[0]
+	player.play("run")
+	player.onUpdate(() => {
+
+		if (player.pos.x < 953) {
+			if (player.pos.y < 485) {
+				camPos(953,485)		
+			} else if (player.pos.y > 796) {
+				camPos(953,796)		
+			}else {
+				camPos(953,player.pos.y)
+			}
+				
+		}
+		else if (player.pos.x > 2230) {
+			if (player.pos.y < 485) {
+				camPos(2230,485)		
+			} else if (player.pos.y > 796) {
+				camPos(2230,796)		
+			}else {
+				camPos(2230,player.pos.y)
+			}
+		} else {
+			if (player.pos.y <= 485) {
+				camPos(player.pos.x, 485)		
+			} else if (player.pos.y >= 796) {
+				camPos(player.pos.x, 796)		
+			}else {
+				camPos(player.pos)
+			}
+		}
+	  })
+
+	
+	function addDialog() {
+		const h = 160
+		const pad = 16
+		const bg = add([
+			pos(0, height() - h),
+			rect(width(), h),
+			color(0, 0, 0),
+			z(100),
+		])
+		const txt = add([
+			text("", {
+				width: width(),
+			}),
+			pos(0 + pad, height() - h + pad),
+			z(100),
+		])
+		bg.hidden = true
+		txt.hidden = true
+		return {
+			say(t) {
+				txt.text = t
+				bg.hidden = false
+				txt.hidden = false
+			},
+			dismiss() {
+				if (!this.active()) {
+					return
+				}
+				txt.text = ""
+				bg.hidden = true
+				txt.hidden = true
+			},
+			active() {
+				return !bg.hidden
+			},
+			destroy() {
+				bg.destroy()
+				txt.destroy()
+			},
+		}
+	}
+
+	let hasKey = false
+	const dialog = addDialog()
+
+	player.onCollide("key", (key) => {
+		destroy(key)
+		hasKey = true
+	})
+
+	player.onCollide("door", () => {
+		if (hasKey) {
+			if (levelIdx + 1 < levels.length) {
+				music.stop()
+				go("main", levelIdx + 1)
+			} else {
+				music.stop()
+				go("win")
+			}
+		} else {
+			dialog.say("you got no key!")
+		}
+	})
+
+
+	player.onCollide("character", (ch) => {
+		dialog.say(ch.msg)
+	})
+
+	const dirs = {
+		"left": LEFT,
+		"right": RIGHT,
+		"up": UP,
+		"down": DOWN,
+	}
+
+	for (const dir in dirs) {
+		onKeyPress(dir, () => {
+			dialog.dismiss()
+		})
+		onKeyDown(dir, () => {
+			player.move(dirs[dir].scale(SPEED))
+		})
+	}
+
+})
+
+scene("win", () => {
+	add([
+		text("You Win!"),
+		pos(width() / 2, height() / 2),
+		origin("center"),
+	])
+})
 
 go('credits-0')
